460a4cdb8b5c21fde54271891b1ac6d343a87f4e
yos <138175440+nushovorkaem@users.noreply.github.com>
2024-01-19 02:22:04 +0100

    Create Forhr.py

---
+++ Forhr.py
@@ -0,0 +1,15 @@
+countr=0
+
+while countr<=99999999999:
+    
+    
+    for i in range (1,4):
+        if i <5:
+            print('I LOVE YOU', end='    ')
+        else: break
+        
+    
+    for i in range (1,7):
+        print('<3', end='    ')    
+        
+    countr+=1    
